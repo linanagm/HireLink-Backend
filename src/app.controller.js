180644c3d26d74e7c1a4095bb41a57c0b59fe7c2
linanagm <lina.nagm@gmail.com>
2025-11-19 09:59:41 +0300

    Task[3][4] - Added Helmet & Rate Limit middlewares in app.controller - route prefix api/

--- src/app.controller.js
+++ src/app.controller.js
@@ -12,16 +12,18 @@ import cookieParser from "cookie-parser";
 import path from "node:path";
 import { ServiceError } from "./Utils/serviceError.utils.js";
 import morgan from "morgan";
+import helmet from "helmet";
+import { rateLimit } from "express-rate-limit";
 
 const bootstrap = async (app , express) => {
     
     // ✅ Middlewares
     app.use(express.json());
 
+    app.use(helmet())
 
     //✅ Cookie parser
     app.use(cookieParser());
-
 
     //✅ CORS -> Cross-Origin Resource Sharing - 
     app.use(cors());
@@ -32,20 +34,28 @@ const bootstrap = async (app , express) => {
     // connect to database
     await connectDB();
     
+    //✅ Rate limit middleware
+    const limiter = rateLimit({
+        windowMs: 15 * 60 * 1000, // 15 minutes - MS=miliseconds - 60*1000=1 min
+        limit: 100, // limit each IP to 100 requests per windowMs
+        standardHeaders: true, 
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+        message: {
+          statusCode : 429,
+          message: "Too many requests from this IP, please try again after 15 minutes"
+        },
+  
+      });
+    app.use(limiter);
+
+
     // req
     //https://localhost:4000/uploads/profile//1762773416814__0.03491477427147682__lofi-girl-painting-3840x2160-21476.jpg"
     app.use("/uploads", express.static(path.resolve("./src/uploads")));
 
 
     /************************ APIs******************/
-    // - auth -> sognup / login / logout / auth me 
-    // - users -> update profile / get profile / upload profile photo / delete account (soft delete) 
-    // - applicant -> dashbord / applications / apply for job
-    // - company -> dashboard / create company / get company / update company / delete company
-    // - admin -> dashboard / get all users / get all companies / delete user (hard delete)
-    // - jobs -> create job(company only) / get all jobs / get job by id / update job(company only) / delete job(company only)
-    // - applications -> get all applications(company only) / get application by id / update application status(company only)
-    // - notifications -> get all notifications / read notification / post notification(admin or system trigger)
+    
     /**********************************************/
     
     
@@ -53,22 +63,20 @@ const bootstrap = async (app , express) => {
     // register / log in / log out / get current user 
     app.use("/api/auth" , authRouter);
     
-    
     app.use("/api/user" , userRouter);
     
     //get profile/ update profile/ upload profile photo 
     app.use("/api/profile", profileRouter)
     
-
     app.use("/api/admin" , adminRouter);
     
-
     app.use("/api/company" , companyRouter);
-    
-
+     
     app.use("/api/jobs", jobsRouter );
 
     app.use("/api/notifications", notificationRouter); // ✅ Route للـ Notifications
+
+
 
     // 404 error Route Not Found
     app.all("/*" , (req , res, next) => {
@@ -80,3 +88,25 @@ const bootstrap = async (app , express) => {
 }
 
 export default bootstrap;
+
+
+
+/***************** Security Middleware Notes  **************************/ 
+//Helmet Explain:
+//Headers
+//attacks
+//1-xss --> cross site scripting -> inject script in response
+//2-mime sniffing --> server mime type file -> browser try to guess mime type -> js -> xss
+//3-clickjacking --> <iframe src=""></iframe>
+  //-buttons ----> pay now  
+
+
+//Rate limiting Explain:
+
+//Denial of service ---> hacker --> drop bandwidth for server 
+// - floating attack ->send many attacksv
+// - resource exhaustion -> send many requests consume server resources 
+//=> express rate limit - web application firewall - request cashing
+
+//brute force attack --> email -> password -> data -> tables -> patterns
+//=> express rate limit - account lock - capatcha - 2 factor authentication - strong password policy
